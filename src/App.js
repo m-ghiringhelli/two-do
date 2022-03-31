21c96daa7bfc4786e30252574ff7b05381de9e56
m-ghiringhelli <mghiring@gmail.com>
2022-03-31 16:26:40 -0700

    added browser router and todos page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { useState } from 'react';
 import './App.css';
 import Auth from './views/Auth/Auth';
+import Todos from './views/Todos/Todos';
+import { getUser } from './services/users';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(getUser());
   return (
-    <div className="App">
-      <Auth />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route exact path='/'>
+            <Auth />
+          </Route>
+          <Route path='/todos'>
+            <Todos />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
