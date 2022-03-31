e2ed4d3ec275b68f0de586bd7a8f179103dd1942
m-ghiringhelli <mghiring@gmail.com>
2022-03-31 16:09:31 -0700

    supabase key

--- src/views/Auth/Auth.js
+++ src/views/Auth/Auth.js
@@ -3,10 +3,15 @@ import React, { useState } from 'react';
 export default function Auth() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log('submitted');
+  };
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           email:
           <input type='email' onChange={(e) => setEmail(e.target.value)}></input>
@@ -15,6 +20,7 @@ export default function Auth() {
           password:
           <input type='password' onChange={(e) => setPassword(e.target.value)}></input>
         </label>
+        <button>submit</button>
       </form>
     </div>
   );
